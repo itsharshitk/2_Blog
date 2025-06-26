8742390232dca84d049f3c492333673bedead778
harshitknp <harshitk2@chetu.com>
2025-06-26 23:13:09 +0530

    creating user model

--- models/authModel.go
+++ models/authModel.go
@@ -1,5 +1,9 @@
 package models
 
 type User struct {
-	ID uint `gorm:"primaryKey" json:"id"`
+	ID       uint   `gorm:"primaryKey" json:"id"`
+	FName    string `json:"fname" validate:"required,max=50,min=2"`
+	LName    string `json:"lname" validate:"required,max=50,min=2"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,validPass"`
 }
